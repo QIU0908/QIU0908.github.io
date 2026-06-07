a8ff021087062ace5a283fe8b5d47abb0842c308
site-QIU0908 <41123119@nfu.edu.tw>
2026-06-07 15:23:36 +0800

    add machine learing 1st

---
+++ machine_learning/competition/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+print("numpy:", np.__version__)
+print("pandas:", pd.__version__)
+print("全部套件載入成功！")
